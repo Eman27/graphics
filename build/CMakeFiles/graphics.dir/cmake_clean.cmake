131de977a1486b522b2fdb95a5934cd731c05600
Mark Emanuele <mark.emanuele.272@gmail.com>
2026-02-03 15:58:10 -0500

    initial commit. Album Spun: Death - Leprosy

---
+++ build/CMakeFiles/graphics.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/graphics.dir/src/glad.c.o"
+  "CMakeFiles/graphics.dir/src/glad.c.o.d"
+  "CMakeFiles/graphics.dir/src/main.cpp.o"
+  "CMakeFiles/graphics.dir/src/main.cpp.o.d"
+  "graphics"
+  "graphics.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/graphics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
